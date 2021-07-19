bf5840654fa67c0222ececbeeca593bef50b94ca
Asheekaur <asheedhiman55@gmail.com>
2021-07-20 00:22:20 +0530

    back button added

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const homePage = document.querySelector(".home-page");
 const rulesBtn = document.querySelector(".rules-btn");
 const rulesPage = document.querySelector(".rules-page");
 const startBtn = document.querySelector(".start-btn");
+const backBtn = document.querySelector(".back-btn");
 
 rulesBtn.addEventListener("click", e => {
     e.preventDefault();
 
     homePage.style.display = 'none';
     rulesPage.style.display = 'block';
+});
+
+
+backBtn.addEventListener("click", e => {
+    e.preventDefault();
+    
+    homePage.style.display = 'block';
+    rulesPage.style.display = 'none';
 });
