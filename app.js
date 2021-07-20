327206f46cc6b37dc8c92208e69420fecf411b6e
Asheekaur <asheedhiman55@gmail.com>
2021-07-20 20:58:51 +0530

    nav and back button of game-page completed

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ const homePage = document.querySelector(".home-page");
 const rulesBtn = document.querySelector(".rules-btn");
 const rulesPage = document.querySelector(".rules-page");
 const startBtn = document.querySelector(".start-btn");
-const backBtn = document.querySelector(".back-btn");
+const backBtn = document.querySelector(".back-btn-1");
+const gamePage = document.querySelector(".game-page");
+const RulesBackBtn = document.querySelector(".back-btn-2");
 
 rulesBtn.addEventListener("click", e => {
-    e.preventDefault();
-
     homePage.style.display = 'none';
     rulesPage.style.display = 'block';
 });
 
-
 backBtn.addEventListener("click", e => {
-    e.preventDefault();
-    
     homePage.style.display = 'block';
     rulesPage.style.display = 'none';
+});
+
+startBtn.addEventListener("click", e => {
+    gamePage.style.display = 'block';
+    rulesPage.style.display = 'none';
+});
+
+RulesBackBtn.addEventListener("click", e => {
+    gamePage.style.display = 'none';
+    rulesPage.style.display = 'block';
 });
